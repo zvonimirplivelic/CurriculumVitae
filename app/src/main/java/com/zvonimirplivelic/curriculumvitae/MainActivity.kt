1fd9bc30129b3fdebd0b9cb0ac91897bcd071159
Zvonimir <zvonimirplivelic91@gmail.com>
2021-03-19 18:23:35 +0100

    master merge attempt

--- app/src/main/java/com/zvonimirplivelic/curriculumvitae/MainActivity.kt
+++ app/src/main/java/com/zvonimirplivelic/curriculumvitae/MainActivity.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import android.transition.AutoTransition
 import android.transition.TransitionManager
 import android.view.View
-import android.view.animation.Animation
-import android.view.animation.RotateAnimation
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
